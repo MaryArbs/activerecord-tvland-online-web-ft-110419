0618270b6b6836335153ba8903dab5785951e3b3
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-19 21:13:13 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,8 @@ class Show < ActiveRecord::Base
   has_many :actors, through: :characters
   belongs_to :network
 
-  def actors_list
+  def actors_list #has an #actors_list method that returns a list of the full names of each actor associated
+ with the show
     self.actors.collect do |actors|
       actors.full_name
    end
