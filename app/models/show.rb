4cf07ccfc9f92ed4f821ed8241e48f472127e82d
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-19 20:24:07 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   has_many :characters
 
   def actors_list
-    Show.all.collect do |show|
+    self.all.collect do |show|
       "#{Actor.all.name}"
     end
   end
