d91f5e1eacf81c66225fa8831fcb798cbccac915
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-19 21:03:00 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,8 @@ class Show < ActiveRecord::Base
 
   def actors_list
     self.characters.collect do |characters|
-      characters.actor_id
+      "{characters.actor_id}"
+      
    end
   end
 end
