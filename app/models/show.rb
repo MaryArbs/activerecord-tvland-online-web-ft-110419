2b5b7d8ca05d63fd10f0dd358e0cbf85153e899d
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-19 21:11:42 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,8 +1,11 @@
 
 class Show < ActiveRecord::Base
 
-  belongs_to :network
+  # belongs_to :network
+  # has_many :characters
   has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
 
   def actors_list
     self.characters.collect do |characters|
