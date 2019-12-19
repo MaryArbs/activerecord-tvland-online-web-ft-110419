a20df170e0f99e4af3175637b15014db7e0f68be
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-19 20:05:50 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,7 +6,9 @@ class Actor < ActiveRecord::Base
  end
 
  def list_roles
-   "#{self.characters} - #{self.shows}"
- end
+   self.characters.collect do |character|
+     "#{character.name} - #{character.show.name}"
+    end.join
+  end
 
 end
